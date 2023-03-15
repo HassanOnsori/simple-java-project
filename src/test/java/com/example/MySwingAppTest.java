722dd60af456116ee5926f2ba93a8d2cf8fb6377
HassanOnsori <106251927+HassanOnsori@users.noreply.github.com>
2023-03-15 15:32:18 +0100

    Update MySwingAppTest.java

--- src/test/java/com/example/MySwingAppTest.java
+++ src/test/java/com/example/MySwingAppTest.java
@@ -1,16 +1,14 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
+import static org.junit.Assert.assertEquals;
 
-
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class MySwingAppTest {
 
-
-// Configuration to run in a headless environment
-    @BeforeAll
+    // Configuration to run in a headless environment
+    @BeforeClass
     public static void setUpClass() {
         System.setProperty("java.awt.headless", "false");
     }
