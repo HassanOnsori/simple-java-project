a8b630b3f4a485edb07d94fe361896143514b608
HassanOnsori <106251927+HassanOnsori@users.noreply.github.com>
2023-03-15 13:19:01 +0100

    Update MySwingAppTest.java

--- src/test/java/com/example/MySwingAppTest.java
+++ src/test/java/com/example/MySwingAppTest.java
@@ -1,21 +1,40 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import java.awt.GraphicsEnvironment;
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
+@ExtendWith(HeadlessExtension.class)
 public class MySwingAppTest {
 
+    private MySwingApp app;
 
-// Configuration to run in a headless environment
-   @BeforeClass
-   public static void setupHeadlessMode() {
-       System.setProperty("java.awt.headless", "true");
-   }
+    @BeforeAll
+    public static void setUp() {
+        Assertions.assertTrue(GraphicsEnvironment.isHeadless());
+    }
 
     @Test
     public void testLabel() {
-        MySwingApp app = new MySwingApp();
-        assertEquals("Hello, Swing!", app.label.getText());
+        app = new MySwingApp();
+        String expectedText = "Hello, world!";
+        String actualText = app.getLabelText();
+        Assertions.assertEquals(expectedText, actualText);
+    }
+}
+
+class HeadlessExtension implements ExecutionCondition {
+    @Override
+    public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
+        return GraphicsEnvironment.isHeadless()
+                ? ConditionEvaluationResult.enabled("Headless environment detected")
+                : ConditionEvaluationResult.disabled("Test requires a headless environment");
     }
 }
