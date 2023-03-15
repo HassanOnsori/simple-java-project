7e5ed15a735dc31cfbec13ef0ce888400b81dab2
Hassan Onsori Delicheh <hassan.onsoridelicheh@umons.ac.be>
2023-03-15 11:49:49 +0100

    add headless confirguration

--- src/test/java/com/example/MySwingAppTest.java
+++ src/test/java/com/example/MySwingAppTest.java
@@ -1,11 +1,19 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.BeforeAll;
 
 
 import org.junit.jupiter.api.Test;
 
 public class MySwingAppTest {
+
+
+// Configuration to run in a headless environment
+//    @BeforeAll
+//    public static void setUpClass() {
+//        System.setProperty("java.awt.headless", "true");
+//    }
 
     @Test
     public void testLabel() {
